e3c528f6638da2353db2a73c660a7a369916c39d
Craig Zeise <craig.zeise@gmail.com>
2016-11-02 19:30:07 -0500

    Update test to use appropriate url

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
+  test "should get signup" do
+    get(signup_path)
     assert_response :success
   end
 
