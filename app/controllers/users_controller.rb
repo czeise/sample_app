f9be5a7711874f9ed15f5394f9fc32daef2f8139
Craig Zeise <craig.zeise@gmail.com>
2017-02-15 21:41:08 -0600

    Add user signup integration test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a succesful save
+      # Handle a successful save
     else
       render 'new'
     end
