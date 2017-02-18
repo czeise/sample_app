092d1a155ca9e760641674fa4a366f3f28c16df3
Craig Zeise <craig.zeise@gmail.com>
2017-02-18 13:18:04 -0600

    Add log out functionality and tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
-      log_in(user)
-      redirect_to(user)
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render('new')
+      render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,15 +16,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test 'login with valid information' do
+  test 'login with valid information followed by logout' do
+    # Log in
     get(login_path)
     post(login_path, params: { session: { email: @user.email,
                                           password: 'password' } })
+    assert logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+
+    # Log out
+    delete logout_path
+    assert_not logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
