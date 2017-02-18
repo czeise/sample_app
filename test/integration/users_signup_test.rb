fe7e73627d3127ac131dec6da5e9b93f7362bf8e
Craig Zeise <craig.zeise@gmail.com>
2017-02-18 12:38:03 -0600

    Login upon signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test 'invalid signup information' do
-    get(signup_path)
+    get signup_path
 
     assert_no_difference "User.count" do
       post(signup_path, params: { user: { name: '', email: 'user@invalid',
@@ -11,10 +11,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password_confirmation: 'bar' } })
     end
 
-    assert_template('users/new')
-    assert_select('div#error_explanation')
-    assert_select('div.field_with_errors')
-    assert_select('form[action="/signup"]')
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_select 'form[action="/signup"]'
   end
 
   test 'valid signup information' do
@@ -26,8 +26,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: 'password' } })
     end
     follow_redirect!
-    assert_template('users/show')
-    assert_not(flash.nil?)
-    assert_select('div.alert.alert-success', 'Welcome to the Sample App!')
+    assert_template 'users/show'
+    assert_not flash.nil?
+    assert_select 'div.alert.alert-success', 'Welcome to the Sample App!'
+    assert logged_in?
   end
 end
