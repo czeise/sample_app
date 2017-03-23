52e300613468a7e09895c6aebc75598159b4ca7f
Craig Zeise <craig.zeise@gmail.com>
2017-03-23 16:59:00 -0500

    Add ability to successfully edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,8 +25,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle successful update
-      # redirect_to(@user)
+      flash[:success] = 'Profile updated'
+      redirect_to(@user)
     else
       render('edit')
     end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -15,4 +15,24 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template('users/edit')
     assert_select('div.alert', 'The form contains 4 errors.')
   end
+
+  test 'successful edit' do
+    new_name = 'Foo Bar'
+    new_email = 'foo@bar.com'
+
+    get(edit_user_path(@user))
+    assert_template('users/edit')
+    patch(user_path(@user), params: { user: { name: new_name,
+                                              email: new_email,
+                                              password: '',
+                                              password_confirmation: '' } })
+
+    assert_not(flash.empty?) # Better to check for the successful flash message?
+    assert_redirected_to(@user)
+
+    # Must reload the user to get the updates
+    @user.reload
+    assert_equal(new_name, @user.name)
+    assert_equal(new_email, @user.email)
+  end
 end
