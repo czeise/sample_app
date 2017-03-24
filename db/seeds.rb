55cc89964637cbeff49b9ab62a5df1da9adab682
Craig Zeise <craig.zeise@gmail.com>
2017-03-24 11:14:49 -0500

    Add admini attribute to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 password = 'password'
 User.create!(name: 'Example User', email: 'example@railstutorial.org',
-             password: password, password_confirmation: password)
+             password: password, password_confirmation: password, admin: true)
 
 99.times do |n|
   name = Faker::Name.name
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -45,4 +45,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert(flash.empty?)
     assert_redirected_to(root_url)
   end
+
+  test 'admin attribute should not be editable through the web' do
+    log_in_as(@other_user)
+    assert_not(@other_user.admin?)
+    patch(user_path(@other_user),
+          params: { user: { password: 'password',
+                            password_confirmation: 'password', admin: true } })
+    assert_not(@other_user.reload.admin?)
+  end
 end
