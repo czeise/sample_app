8e7ad9bc41934a13e689852040008b8829bc18bb
Craig Zeise <craig.zeise@gmail.com>
2017-03-24 12:56:53 -0500

    Add destroy tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,4 +54,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                             password_confirmation: 'password', admin: true } })
     assert_not(@other_user.reload.admin?)
   end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete(user_path(@user))
+    end
+    assert_redirected_to(login_url)
+  end
+
+  test 'should redirect destroy when logged in as non-admin' do
+    log_in_as(@user)
+    assert_no_difference 'User.count' do
+      delete(user_path(@user))
+    end
+    assert_redirected_to(root_url)
+  end
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,15 +3,46 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:one)
+    @admin = users(:admin)
+    @user_two = users(:two)
   end
 
-  test 'index including pagination' do
-    log_in_as(@user)
+  test 'index as admin including pagination and delete links' do
+    log_in_as(@admin)
     get(users_path)
     assert_template('users/index')
     assert_select('div.pagination', count: 2)
     User.paginate(page: 1).each do |user|
       assert_select('a[href=?]', user_path(user), text: user.name)
+      unless user == @admin
+        assert_select('a[href=?]', user_path(user), text: 'delete',
+                                                    method: :delete)
+      end
+    end
+
+    # Admin can delete users...
+    assert_difference 'User.count', -1 do
+      delete(user_path(@user))
+    end
+  end
+
+  test 'index as non-admin' do
+    log_in_as(@user)
+    get(users_path)
+    assert_template('users/index')
+    assert_select('div.pagination', count: 2)
+
+    User.paginate(page: 1).each do |user|
+      assert_select('a[href=?]', user_path(user), text: user.name)
+
+      # Non-admins shouldn't see 'delete' links
+      assert_select('a[href=?]', user_path(user), text: 'delete',
+                                                  method: :delete, count: 0)
+    end
+
+    # Non-admins can't delete users
+    assert_no_difference 'User.count' do
+      delete(user_path(@user_two))
     end
   end
 end
