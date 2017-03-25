845bf09c0a62cfe6551e157f13d7f95b546b6db0
Craig Zeise <craig.zeise@gmail.com>
2017-03-25 14:26:24 -0500

    Create account activation data model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  # Setting a smaller default for pagination (looks better on the index page)
-  self.per_page = 12
+  before_create :create_activation_digest
+  before_save :downcase_email
 
-  before_save { email.downcase! }
   validates(:name, presence: true, length: { maximum: 50 })
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates(:email, presence: true, length: { maximum: 255 },
@@ -12,6 +11,9 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false })
   has_secure_password
   validates(:password, presence: true, length: { minimum: 6 }, allow_nil: true)
+
+  # Setting a smaller default for pagination (looks better on the index page)
+  self.per_page = 12
 
   # Remembers a user in the database for use in persistent sessions
   def remember
@@ -48,5 +50,18 @@ class User < ApplicationRecord
   # Returns a random token
   def self.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  private
+
+  # Converts email to all lower-case
+  def downcase_email
+    email.downcase!
+  end
+
+  # Creates and assigns the activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,13 @@
 
 password = 'password'
 User.create!(name: 'Example User', email: 'example@railstutorial.org',
-             password: password, password_confirmation: password, admin: true)
+             password: password, password_confirmation: password, admin: true,
+             activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
   User.create!(name: name, email: email,
-               password: password, password_confirmation: password)
+               password: password, password_confirmation: password,
+               activated: true, activated_at: Time.zone.now)
 end
