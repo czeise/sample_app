1f6b882c73ce6c96fff2e3661e0952be53dfa45b
Craig Zeise <craig.zeise@gmail.com>
2017-04-07 16:25:55 -0500

    Generate password reset controller and add link to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root('static_pages#home')
   get('/help', to: 'static_pages#help')
@@ -12,4 +11,5 @@ Rails.application.routes.draw do
   delete('/logout', to: 'sessions#destroy')
   resources(:users)
   resources(:account_activations, only: [:edit])
+  resources(:password_resets, only: %i[new create edit update])
 end
