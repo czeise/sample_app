d3b65de1a7f73d3dece619cc037bb430c4260426
Craig Zeise <craig.zeise@gmail.com>
2017-04-07 16:41:45 -0500

    Create password reset migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325190910) do
+ActiveRecord::Schema.define(version: 20170407213254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170325190910) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_set_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
