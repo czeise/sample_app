feed89b3e89f5fc19f9aba902a2b3a2dee9bb5b2
Craig Zeise <craig.zeise@gmail.com>
2017-04-08 15:14:40 -0500

    Update password reset test and re-enable

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,12 +15,14 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
-    skip('Need to finish password reset functionality')
-    mail = UserMailer.password_reset
+    user = users(:one)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match(user.reset_token, mail.body.encoded)
+    assert_match(user.email, mail.body.encoded)
   end
 
 end
