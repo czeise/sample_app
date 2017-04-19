60d1a1d73d02665f29cfcb861175df18a546934d
Craig Zeise <craig.zeise@gmail.com>
2017-04-18 22:45:58 -0500

    Seed microposts and style the user page

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,9 @@ User.create!(name: 'Example User', email: 'example@railstutorial.org',
                password: password, password_confirmation: password,
                activated: true, activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::ChuckNorris.fact[0...140]
+  users.each { |user| user.microposts.create!(content: content) }
+end
