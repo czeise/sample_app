0c57b7e2a034d1bf5f02b981f4009ae97196498a
Craig Zeise <craig.zeise@gmail.com>
2017-04-20 16:48:06 -0500

    Give user the ability to create microposts on the site

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    return unless logged_in?
+    @micropost = current_user.microposts.build
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   def help
