178c84abc54cfefbe1cf2c6a6324ea3063096f9a
Craig Zeise <craig.zeise@gmail.com>
2017-06-01 16:10:53 -0500

    Add following and unfollowing tests

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:one)
     log_in_as(@user)
+    @other = users(:user_5)
   end
 
   test "following page" do
@@ -24,4 +25,31 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "should follow a user the standard way" do
+    assert_difference('@user.following.count', 1) do
+      post(relationships_path, params: { followed_id: @other.id })
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference('@user.following.count', 1) do
+      post(relationships_path, xhr: true, params: { followed_id: @other.id })
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference('@user.following.count', -1) do
+      delete(relationship_path(relationship))
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference('@user.following.count', -1) do
+      delete(relationship_path(relationship), xhr: true)
+    end
+  end
 end
