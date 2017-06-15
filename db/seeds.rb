3637839e5ff591b047846d4c52bc20652d68fe32
Craig Zeise <craig.zeise@gmail.com>
2017-06-14 22:07:12 -0500

    Production database doesn't need fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,25 +12,27 @@ User.create!(name: 'Example User', email: 'example@railstutorial.org',
              password: password, password_confirmation: password, admin: true,
              activated: true, activated_at: Time.zone.now)
 
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n + 1}@railstutorial.org"
-  User.create!(name: name, email: email,
-               password: password, password_confirmation: password,
-               activated: true, activated_at: Time.zone.now)
-end
+unless Rails.env == 'production'
+  99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n + 1}@railstutorial.org"
+    User.create!(name: name, email: email,
+                 password: password, password_confirmation: password,
+                 activated: true, activated_at: Time.zone.now)
+  end
 
-# Microposts
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::ChuckNorris.fact[0...140]
-  users.each { |user| user.microposts.create!(content: content) }
-end
+  # Microposts
+  users = User.order(:created_at).take(6)
+  50.times do
+    content = Faker::ChuckNorris.fact[0...140]
+    users.each { |user| user.microposts.create!(content: content) }
+  end
 
-# Following relationships
-users = User.all
-user = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+  # Following relationships
+  users = User.all
+  user = users.first
+  following = users[2..50]
+  followers = users[3..40]
+  following.each { |followed| user.follow(followed) }
+  followers.each { |follower| follower.follow(user) }
+end
